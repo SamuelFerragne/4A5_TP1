e30bc25f4439c4a35950c0dbbd9a45bc4f913baa
Samuel Ferragne <xavier.racine2018@gmail.com>
2023-03-12 16:51:38 -0400

    usestate_prof

--- src/prof/pages/Profs.js
+++ src/prof/pages/Profs.js
@@ -1,17 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import ListeProfs from "../components/ListeProfs";
-import ListeCours from "../../cours/components/ListeCours";
 
 function Profs() {
-  const profs = [
+  var defaultImage =
+    "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.pinclipart.com%2Fpicdir%2Fbig%2F157-1578186_user-profile-default-image-png-clipart.png&f=1&nofb=1&ipt=43bbb9448ee33cb8d0045e45c010ad282a8b74dafe2cee5fae738d6b65ec1eca&ipo=images";
+  const [profs, setProfs] = useState([
     {
       id: "u1",
       nom: "Sylvain Labranche",
-      image:
-        "https://www.chumontreal.qc.ca/sites/default/files/guy-pare_5.jpeg",
-      cours: [ListeCours],
+      image: defaultImage,
     },
-  ];
+  ]);
+
+  function ajouterProf(nouveauProf) {
+    setProfs(profs.concat(nouveauProf));
+    console.log(profs);
+  }
+
   return <ListeProfs profs={profs} />;
 }
 
